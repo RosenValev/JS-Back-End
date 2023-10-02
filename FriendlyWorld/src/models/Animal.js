32b9f66fb3a7d59a8f415a68063c7ac7df84c8a3
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-10-02 12:05:12 +0300

    Models added

---
+++ FriendlyWorld/src/models/Animal.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+const animalSchema = new mongoose.animalSchema({
+    name: {
+        type: String,
+        required: [true, 'Name is required!'],
+        minLength: [3, 'characters required minimum with 3 length'],
+        maxLength: [50, 'characters required maximum with 50 length'],
+    },
+    years: {
+        type: Number,
+        required: [true, 'Years are required!'],
+        min: [0, 'Years should be a possitive number'],
+    },
+    kind: {
+        type: String,
+        required: [true, 'Kind is required!'],
+        minLength: [3, 'characters required minimum with 3 length'],
+        maxLength: [50, 'characters required maximum with 50 length'],
+    },
+    image: {
+        type: String,
+        requred: [true, 'Image is required!'],
+    },
+    need: {
+        type: String,
+        required: [true, 'Need is required!'],
+        minLength: [3, 'characters required minimum with 3 length'],
+        maxLength: [50, 'characters required maximum with 50 length'],
+    },
+    location: {
+        type: String,
+        required: [true, 'Location is required!'],
+        minLength: [3, 'characters required minimum with 3 length'],
+        maxLength: [50, 'characters required maximum with 50 length'],
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required!'],
+        minLength: [3, 'characters required minimum with 3 length'],
+        maxLength: [500, 'characters required maximum with 500 length'],
+    },
+    donations: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }],
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+const Animal = mongoose.model('Animal', animalSchema);
+
+module.exports = Animal;
+
---
+++ FriendlyWorld/src/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.userSchema({
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        match: [/^[A-Za-z0-9]+$/, 'Email must be alphanumeric'],
+        validate: {
+            validator: async function (value) {
+                // Custom validation logic to check uniqueness
+                const email = await this.constructor.findOne({ email: value });
+                return !email;
+            },
+            message: 'Email already exists',
+        },
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+    }
+});
+
+userSchema.virtual('repeatPassword')
+    .set(function (value) {
+        if (value !== this.password) {
+            throw new Error('Password don`t match');
+        }
+    });
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
+
