e88563b13926d0294d4fb88c58e3a0a9bcfdeffb
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-10-06 12:09:59 +0300

    Edit functionality

--- Petsagram/src/controllers/photoController.js
+++ Petsagram/src/controllers/photoController.js
@@ -1,27 +1,28 @@
 const router = require('express').Router();
 const photoService = require('../services/photoService.js');
 const { extractErrorMessage } = require('../utils/errorHelper.js');
+const { isAuth } = require('../middlewares/authorizationMiddleware.js')
+
 
 //CATALOG
 router.get('/catalog', async (req, res) => {
     try {
         const photos = await photoService.getAllPhotos().lean();
         res.render('photos/catalog', { photos });
-        console.log(photos)
 
     } catch (err) {
         const errorMessages = extractErrorMessage(err);
-        res.render('photos/catalog', { errorMessages })
+        res.render('photos/catalog', { errorMessages });
     }
 });
 
 
 //CREATE
-router.get('/create', (req, res) => {
+router.get('/create', isAuth, (req, res) => {
     res.render('photos/create')
 });
 
-router.post('/create', async (req, res) => {
+router.post('/create', isAuth, async (req, res) => {
     const { name, age, description, location, image } = req.body;
 
     try {
@@ -34,11 +35,57 @@ router.post('/create', async (req, res) => {
             owner: req.user._id,
         });
 
-        res.redirect('/photos/catalog')
+        res.redirect('/photos/catalog');
 
     } catch (err) {
         const errorMessages = extractErrorMessage(err);
         res.render('photos/create', { errorMessages, name, age, description, location, image });
+    }
+});
+
+//DETAILS
+router.get('/:photoId/details', async (req, res) => {
+    const photoId = req.params.photoId;
+    let isOwner = false;
+    // const userId = req.user?._id; 
+
+    try {
+        const photo = await photoService.getById(photoId).lean();
+        isOwner = req.user?._id === photo.owner._id.toString();
+        res.render('photos/details', { photo, isOwner });
+
+    } catch (err) {
+        const errorMessages = extractErrorMessage(err);
+        res.render('photos/details', { errorMessages });
+    }
+});
+
+
+//EDIT
+router.get('/:photoId/edit', isAuth, async (req, res) => {
+    const photoId = req.params.photoId;
+
+    try {
+        const photo = await photoService.getById(photoId).lean();
+        res.render('photos/edit', { photo });
+
+    } catch (err) {
+        const errorMessages = extractErrorMessage(err);
+        res.render('photos/details', { errorMessages });
+    }
+});
+
+router.post('/:photoId/edit', isAuth, async (req, res) => {
+    const photoId = req.params.photoId;
+    const photoData = req.body;
+
+    try {
+        await photoService.edit(photoId, photoData);
+        res.redirect(`/photos/${photoId}/details`);
+
+    } catch (err) {
+        const errorMessages = extractErrorMessage(err);
+        res.render('photos/details', { errorMessages });
     }
 });
 
--- Petsagram/src/services/photoService.js
+++ Petsagram/src/services/photoService.js
@@ -3,3 +3,7 @@ const Photo = require('../models/Photo.js');
 exports.create = (photoData) => Photo.create(photoData);
 
 exports.getAllPhotos = () => Photo.find().populate('owner');
+
+exports.getById = (photoId) => Photo.findById(photoId).populate('owner');
+
+exports.edit = (photoId, photoData) => Photo.findByIdAndUpdate(photoId, photoData);
