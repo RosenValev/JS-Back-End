fa4b314029d22a76777b67ca41f4f4a568ad499b
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-10-12 17:08:39 +0300

    init, register, login, logout functionality

---
+++ Gaming Team/src/controllers/homeController.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { extractErrorMessage } = require('../utils/errorHelper.js');
+
+
+router.get('/', async (req, res) => {
+    res.render('home');
+
+});
+
+
+router.get('/404', (req, res) => {
+    res.render('404');
+});
+
+
+module.exports = router;
---
+++ Gaming Team/src/services/userService.js
@@ -0,0 +1,43 @@
+const User = require('../models/User.js');
+const bcrypt = require('bcrypt');
+const { SECRET } = require('../config/secretConfig.js');
+const jwtPromises = require('../lib/jwt.js');
+
+exports.register = async (userData) => {
+    //Logic for automatic login after register
+    const user = await User.findOne({ username: userData.username });
+    if (user) {
+        throw new Error('Username already exists');
+    }
+
+    const newUser = await User.create(userData);
+    const token = generateToken(newUser);
+    return token;
+};
+
+exports.login = async (email, password) => {
+    const user = await User.findOne({ email });
+    if (!email) {
+        throw new Error('Cannot find email or password');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+        throw new Error('Cannot find email or password');
+    }
+
+    const token = await generateToken(user);
+    return token;
+};
+
+
+async function generateToken(user) {
+    const payload = {
+        _id: user._id,
+        username: user.username,
+        email: user.email
+    }
+
+    const token = jwtPromises.sign(payload, SECRET, { expiresIn: '2d' });
+    return token;
+}
