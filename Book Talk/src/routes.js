0729623c4b4cc3e0067244fbe77918591bb7e935
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-10-14 19:29:07 +0300

    init

---
+++ Book Talk/src/routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const homeController = require('./controllers/homeController.js');
+const userController = require('./controllers/userController.js');
+
+
+router.use(homeController);   // This controller will work all the time.
+router.use('/users', userController);
+
+router.get('*', (req, res) => {  //Redirect everything else to page 404
+    res.redirect('/404')
+});
+
+module.exports = router;
