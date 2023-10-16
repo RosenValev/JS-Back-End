e4a25d78b4e976931998f5aecd11b00d754c655d
RosenValev <121716535+RosenValev@users.noreply.github.com>
2023-10-16 23:42:30 +0300

    init

---
+++ Crypto Trade/src/config/expressConfig.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const cookieParse = require('cookie-parser');
+const { auth } = require('../middlewares/authorizationMiddleware.js');
+
+function ExpressConfig(app) {
+    app.use(express.static(path.resolve(__dirname, '../static')));  // Configure static files folder
+    app.use(express.urlencoded({ extended: false })); // Middleware body parser -> parse to a Obj the request if we had POST data send from FORM, access it from req.body
+    app.use(cookieParse()); //Cookie-parser middleware
+    app.use(auth); //Authorization middleware
+}
+
+module.exports = ExpressConfig;
